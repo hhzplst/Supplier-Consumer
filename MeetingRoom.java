0a815ef6e81bb01e2d9467ad06a877d3f72b604d
Ivy Chang <ivycwq@icloud.com>
2017-02-24 17:02:32 -0800

    making double-checked locking MeetingRoom

--- MeetingRoom.java
+++ MeetingRoom.java
@@ -1,3 +1,16 @@
 public class MeetingRoom {
-  
+  private volatile static MeetingRoom uniqueMeetingRoom;
+
+  private MeetingRoom() {}
+
+  public static MeetingRoom getMeetingRoomInstance() {
+    if (uniqueMeetingRoom == null) {
+      synchronized (MeetingRoom.class) {
+        if (uniqueMeetingRoom == null) {
+          uniqueMeetingRoom = new MeetingRoom();
+        }
+      }
+    }
+    return uniqueMeetingRoom;
+  }
 }
